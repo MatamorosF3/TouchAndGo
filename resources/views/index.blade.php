94fa015fd9fef62d958f095f5f8e945aa0c01f7d
MatamorosF3 <matamorosf3@gmail.com>
2015-11-26 16:08:06 -0600

    Styles

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,9 @@
 @extends('layout.layout')
 
 <link rel="stylesheet" href="assets/css/slider.css" type="text/css" />
-<<link rel="stylesheet" href="assets/css/spinner.css" type ="text/css"/>
+<link rel="stylesheet" href="assets/css/spinner.css" type ="text/css"/>
+<link rel="stylesheet" href="assets/css/circle.css" type ="text/css"/>
+
 
 @section('t', 'Page Title')
 
@@ -33,24 +35,25 @@
 		</figure>
 	</figure>
 </div>
-    
-     <div>
+    <style>
+
+</style>
+     <div id= "circles" >
+        <div id="stage" style="background: rgba(142, 42, 48, 0.6);">
+		<p id="spinner" style="background: rgba(0,1,1,0.5); text-align: center; color: #fff;">¿Qué Quieres Hacer?</p>
+		</div>
             
-            <div id="stage" style="background: rgba(0,0,0,0.5);">
-<p id="spinner" style="background: rgba(0,0,0,0.5); text-align: center; color: #fff;">¿Qué Quieres Hacer?</p>
-</div>
-            
-                <img class="img-circle" src="https://image.freepik.com/free-icon/man-sitting-in-front-of-a-table-eating-and-drinking-while-having-lunch_318-62566.png" />
-                <img class="img-circle" src="http://univers-maya.fr/wp-content/uploads/2013/09/boire.jpg" />
-                <img class="img-circle" src="https://despiertaiglesiaya.files.wordpress.com/2013/02/bailar.jpg" />
-                <img class="img-circle" src="http://www.buscorestaurantes.com/files/logo/Cafe-Sito-II-103683.jpg" />
-                <img class="img-circle" src="http://cdn5.dibujos.net/dibujos/pintar/cine.gif" />
-                <img class="img-circle" src="http://www.idibujos.com/wp-content/uploads/2012/04/Leer.jpg" /> 
-                <img class="img-circle" src="http://blogs.ceibal.edu.uy/formacion/wp-content/uploads/2014/12/turismo.jpg" /> 
+                <img id="imag" class="img-circle" src="https://image.freepik.com/free-icon/man-sitting-in-front-of-a-table-eating-and-drinking-while-having-lunch_318-62566.png" />
+                <img id="imag" class="img-circle" src="http://univers-maya.fr/wp-content/uploads/2013/09/boire.jpg" />
+                <img id="imag" class="img-circle" src="https://despiertaiglesiaya.files.wordpress.com/2013/02/bailar.jpg" />
+                <img id="imag" class="img-circle" src="http://www.buscorestaurantes.com/files/logo/Cafe-Sito-II-103683.jpg" />
+                <img id="imag" class="img-circle" src="http://cdn5.dibujos.net/dibujos/pintar/cine.gif" />
+                <img id="imag" class="img-circle" src="http://www.idibujos.com/wp-content/uploads/2012/04/Leer.jpg" /> 
+                <img id="imag" class="img-circle" src="http://blogs.ceibal.edu.uy/formacion/wp-content/uploads/2014/12/turismo.jpg" /> 
               
             
            
-            <div class="circle-image"></div>
+             <div class="circle-image"></div>
              <div class="circle-image"></div>
              <div class="circle-image"></div>
              <div class="circle-image"></div>
