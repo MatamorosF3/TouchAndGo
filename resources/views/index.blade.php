a1ed9e4cf31928d7eb6508f94796b29e81e7397c
Swamy Jose Antunez <swammj@gmail.com>
2015-12-04 06:55:26 +0000

    css

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 <link rel="stylesheet" href="assets/css/slider.css" type="text/css" />
 <link rel="stylesheet" href="assets/css/spinner.css" type ="text/css"/>
 <link rel="stylesheet" href="assets/css/circle.css?v=2" type ="text/css"/>
-
+<link href='https://fonts.googleapis.com/css?family=Indie+Flower' rel='stylesheet' type='text/css'>
 
 @section('t', 'Page Title')
 
@@ -36,26 +36,6 @@
 	</figure>
 </div>
 
-     <div id= "circles" >
-        <div id="stage" style="background: rgba(142, 42, 48, 0.6);">
-		<p id="spinner" style="background: rgba(0,1,1,0.5); text-align: center; color: #fff;">¿Qué Quieres Hacer?</p>
-		</div>
-            
-                <img id="imag" class="img-circle" src="https://image.freepik.com/free-icon/man-sitting-in-front-of-a-table-eating-and-drinking-while-having-lunch_318-62566.png" />
-                <img id="imag" class="img-circle" src="http://univers-maya.fr/wp-content/uploads/2013/09/boire.jpg" />
-                <img id="imag" class="img-circle" src="https://despiertaiglesiaya.files.wordpress.com/2013/02/bailar.jpg" />
-                <img id="imag" class="img-circle" src="http://www.buscorestaurantes.com/files/logo/Cafe-Sito-II-103683.jpg" />
-                <img id="imag" class="img-circle" src="http://cdn5.dibujos.net/dibujos/pintar/cine.gif" />
-                <img id="imag" class="img-circle" src="http://www.idibujos.com/wp-content/uploads/2012/04/Leer.jpg" /> 
-                <img id="imag" class="img-circle" src="http://blogs.ceibal.edu.uy/formacion/wp-content/uploads/2014/12/turismo.jpg" /> 
-              
-            
-           
-             <div class="circle-image"></div>
-             <div class="circle-image"></div>
-             <div class="circle-image"></div>
-             <div class="circle-image"></div>
-             <div class="circle-image"></div>
-        </div>
+    
     </body>
 @stop
--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -7,7 +7,41 @@
     <body>
 
         <div class="container">
+            
             @yield('content')
+        </div>
+         <div id= "circles" >
+        <div id="stage" style="background: rgba(142, 42, 48, 0.6);">
+		<p id="spinner" style="background: rgba(0,1,1,0.5); text-align: center; color: #fff;">¿Qué Quieres Hacer?</p>
+		</div>
+				<div style="width:120px; float:left; text-align:center;">
+                	<img id="imag" class="img-circle" src="https://image.freepik.com/free-icon/man-sitting-in-front-of-a-table-eating-and-drinking-while-having-lunch_318-62566.png" />
+                	<span class="img-title">Comer</span>
+                </div>
+                <div style="width:120px; float:left; text-align:center;">
+                	<img id="imag" class="img-circle" src="http://univers-maya.fr/wp-content/uploads/2013/09/boire.jpg" />
+                	<span class="img-title">Beber</span>
+                </div>
+                <div style="width:120px; float:left; text-align:center;">
+               		<img id="imag" class="img-circle" src="https://despiertaiglesiaya.files.wordpress.com/2013/02/bailar.jpg" />
+                	<span class="img-title">Bailar</span>
+                </div>
+                <div style="width:120px; float:left; text-align:center;">
+                	<img id="imag" class="img-circle" src="http://www.buscorestaurantes.com/files/logo/Cafe-Sito-II-103683.jpg" />
+                	<span class="img-title">cafe</span>
+                </div>
+		        <div style="width:120px; float:left; text-align:center;">
+		        	<img id="imag" class="img-circle" src="http://cdn5.dibujos.net/dibujos/pintar/cine.gif" />
+		        	<span class="img-title">Cine</span>
+		        </div>
+                <div style="width:120px; float:left; text-align:center;">
+                	<img id="imag" class="img-circle" src="http://www.idibujos.com/wp-content/uploads/2012/04/Leer.jpg" /> 
+                	<span class="img-title">Leer</span>
+                </div>
+                <div style="width:120px; float:left; text-align:center;">
+                	<img id="imag" class="img-circle" src="http://blogs.ceibal.edu.uy/formacion/wp-content/uploads/2014/12/turismo.jpg" /> 
+                	<span class="img-title">Aventura</span>
+                </div>
         </div>
     </body>
 </html>
