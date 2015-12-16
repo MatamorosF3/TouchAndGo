22862021091cab3851898a687a0d9284a559171d
MatamorosF3 <matamorosf3@gmail.com>
2015-12-15 19:03:41 -0600

    link

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -22,31 +22,31 @@
 	        	<p id="spinner">¿Qué Quieres Hacer?</p>
 		    </div>
 				<div class="casilla" style="width:120px; float:left; text-align:center; display: inline-block">
-                	<img id="imag" class="img-circle" src="https://image.freepik.com/free-icon/man-sitting-in-front-of-a-table-eating-and-drinking-while-having-lunch_318-62566.png" />
+                	<a href="tops"><img id="imag" class="img-circle" src="https://image.freepik.com/free-icon/man-sitting-in-front-of-a-table-eating-and-drinking-while-having-lunch_318-62566.png" /> </a>
                 	<span class="img-title">Comer</span>
                 </div>
                 <div class="casilla" style="width:120px; float:left; text-align:center; display: inline-block">
-                	<img id="imag" class="img-circle" src="http://univers-maya.fr/wp-content/uploads/2013/09/boire.jpg" />
-                	<span class="img-title">Beber</span>
+                	<a href="tops"><img id="imag" class="img-circle" src="http://univers-maya.fr/wp-content/uploads/2013/09/boire.jpg" /></a>
+                	<span class="img-title">Beber</span> 
                 </div>
                 <div class="casilla" style="width:120px; float:left; text-align:center; display: inline-block">
-               		<img id="imag" class="img-circle" src="https://despiertaiglesiaya.files.wordpress.com/2013/02/bailar.jpg" />
+               		<a href="tops"><img id="imag" class="img-circle" src="https://despiertaiglesiaya.files.wordpress.com/2013/02/bailar.jpg" /></a>
                 	<span class="img-title">Bailar</span>
                 </div>
                 <div class="casilla"  style="width:120px; float:left; text-align:center; display: inline-block">
-                	<img id="imag" class="img-circle" src="http://www.buscorestaurantes.com/files/logo/Cafe-Sito-II-103683.jpg" />
+                	<a href="tops"><img id="imag" class="img-circle" src="http://www.buscorestaurantes.com/files/logo/Cafe-Sito-II-103683.jpg" /></a>
                 	<span class="img-title">cafe</span>
                 </div>
 		        <div class="casilla" style="width:120px; float:left; text-align:center; display: inline-block">
-		        	<img id="imag" class="img-circle" src="http://cdn5.dibujos.net/dibujos/pintar/cine.gif" />
+		        	<a href="tops"><img id="imag" class="img-circle" src="http://cdn5.dibujos.net/dibujos/pintar/cine.gif" /></a>
 		        	<span class="img-title">Cine</span>
 		        </div>
                 <div class="casilla" style="width:120px; float:left; text-align:center; display: inline-block">
-                	<img id="imag" class="img-circle" src="http://www.idibujos.com/wp-content/uploads/2012/04/Leer.jpg" /> 
+                	<a href="tops"><img id="imag" class="img-circle" src="http://www.idibujos.com/wp-content/uploads/2012/04/Leer.jpg" /> </a>
                 	<span class="img-title">Leer</span>
                 </div>
                 <div class="casilla" style="width:120px; float:left; text-align:center; display: inline-block">
-                	<img id="imag" class="img-circle" src="http://blogs.ceibal.edu.uy/formacion/wp-content/uploads/2014/12/turismo.jpg" /> 
+                	<a href="tops"><img id="imag" class="img-circle" src="http://blogs.ceibal.edu.uy/formacion/wp-content/uploads/2014/12/turismo.jpg" /> </a>
                 	<span class="img-title">Aventura</span>
                 </div>
         </div>
--- resources/views/lista.blade.php
+++ resources/views/lista.blade.php
@@ -13,25 +13,25 @@
     <h1>Top 10 </h1>
 <div id="owl-demo" class="owl-carousel">
     
-     <div> <img src="https://images.unsplash.com/45/aXpQLs4AQ5e1EtmeYkz3_20140524_124026.jpg?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=e5f1c26cff07b0ae690e521c2ac2afa4"></img></div>
+     <div> <a href="details"> <img src="https://images.unsplash.com/45/aXpQLs4AQ5e1EtmeYkz3_20140524_124026.jpg?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=e5f1c26cff07b0ae690e521c2ac2afa4"></img> </a></div>
 
-     <div><img src="https://images.unsplash.com/uploads/1412198485051133af17f/5049dacb?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=c2904a92552e96048553a7fc23cc555d"></img></div>
+     <div><a href="details"><img src="https://images.unsplash.com/uploads/1412198485051133af17f/5049dacb?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=c2904a92552e96048553a7fc23cc555d"></img></a></div>
 
-     <div><img src="https://images.unsplash.com/photo-1421622548261-c45bfe178854?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=44c4af81b220a2c86aecbe96daf40064"></img></div>
+     <div><a href="details"><img src="https://images.unsplash.com/photo-1421622548261-c45bfe178854?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=44c4af81b220a2c86aecbe96daf40064"></img></a></div>
 
-     <div><img src="https://images.unsplash.com/reserve/oHPXcFZQ9SyeGD5F7T1g_ramenya.jpg?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=41918b98e76dda6f9a9e6c54d4a564b8"></img></div>
+     <div><a href="details"><img src="https://images.unsplash.com/reserve/oHPXcFZQ9SyeGD5F7T1g_ramenya.jpg?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=41918b98e76dda6f9a9e6c54d4a564b8"></img></a></div>
 
-    <div><img src="https://images.unsplash.com/17/unsplash_527bf4b4ae00d_1.JPG?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=e7a2744d74d810057947f690d27e1c44"></img></div>
+    <div><a href="details"><img src="https://images.unsplash.com/17/unsplash_527bf4b4ae00d_1.JPG?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=e7a2744d74d810057947f690d27e1c44"></img></a></div>
   
-    <div><img src="https://images.unsplash.com/photo-1426259759666-68da5c54402d?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=1dc395c4829fa10ec4ad4f77627d83c7"></img></div>
+    <div><a href="details"><img src="https://images.unsplash.com/photo-1426259759666-68da5c54402d?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=1dc395c4829fa10ec4ad4f77627d83c7"></img></a></div>
 
-     <div><img src="https://images.unsplash.com/photo-1417353783325-14cb8f9ba1dd?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=aeb8522c0ee11fb1c1ee526511418d18"></img></div>
+     <div><a href="details"><img src="https://images.unsplash.com/photo-1417353783325-14cb8f9ba1dd?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=aeb8522c0ee11fb1c1ee526511418d18"></img></a></div>
 
-     <div><img src="https://images.unsplash.com/photo-1432752641289-a25fc853fceb?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=6893cc1e5e5c178660d798eaba9af94a"></img></div>
+     <div><a href="details"><img src="https://images.unsplash.com/photo-1432752641289-a25fc853fceb?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=6893cc1e5e5c178660d798eaba9af94a"></img></a></div>
 
-     <div><img src="https://images.unsplash.com/photo-1416453072034-c8dbfa2856b5?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=ee750f3ac1d1dced90c374d9e943a925"></img></div>
+     <div><a href="details"><img src="https://images.unsplash.com/photo-1416453072034-c8dbfa2856b5?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=ee750f3ac1d1dced90c374d9e943a925"></img></a></div>
 
-    <div><img src="https://images.unsplash.com/photo-1428515613728-6b4607e44363?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=8a5b923d3abe1cb37a0ecacb878e8cce"></img></div>
+    <div><a href="details"><img src="https://images.unsplash.com/photo-1428515613728-6b4607e44363?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&w=1080&fit=max&s=8a5b923d3abe1cb37a0ecacb878e8cce"></img></a></div>
 
 </div>
 
